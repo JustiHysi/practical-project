c44fdd1b35500ee7bc2c39d25c69c9d67351d220
Justi Hysi <142355907+JustiHysi@users.noreply.github.com>
2023-09-08 02:08:05 +1300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,7 +13,7 @@ public class App
     private static  final  Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
+
         DbManager manager =  DbManager.getInstance();
         logger.info("connected successfully: " + manager.isConnectedSuccessfully());
     }
